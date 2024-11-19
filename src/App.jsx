ef6189f7deaf6295d3eef8d377a4be07499e105a
rithu453 <marpallyrithwik8@gmail.com>
2024-11-19 12:39:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Hom from './com/Home';
+import Sum from './com/Sum';
+import Compare from './com/Compare';
+import Sumresult from './com/Sumresult'; 
+import Nav from './com/Nav';
+import Compres from './com/CompareResult';
+import ComparePrices from './com/Pricecomp';
+
+function App() {
+  return (
+    <Router>
+       <Nav/>
+      <Routes>
+        <Route path="/" element={<Hom />} />
+        <Route path="/summarize" element={<Sum />} />
+        <Route path="/compare" element={<Compare />} />
+        <Route path="/sumresult" element={<Sumresult />} />
+        <Route path="/compres" element={<Compres />} />
+        <Route path="/pricecomp" element={<ComparePrices />} />
+
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
